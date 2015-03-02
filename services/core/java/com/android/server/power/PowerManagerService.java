a5befa844b79c911cb28dc93f1c6978048519e79
Ziga Svetina <ziga@nevron.si>
2015-03-02 14:37:40 +0000

    Screen sleeping is never turned on.

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -1594,7 +1594,9 @@ public final class PowerManagerService extends SystemService
      * to being fully awake or else go to sleep for good.
      */
     private boolean isItBedTimeYetLocked() {
-        return mBootCompleted && !isBeingKeptAwakeLocked();
+        // For now: no sleeping of screen, always ON
+        return false;
+        //return mBootCompleted && !isBeingKeptAwakeLocked();
     }
 
     /**
